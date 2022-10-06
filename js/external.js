37f4a28a95c150bfb9a0af52b939a6dd9f4d3535
Brandon Hall <bran.tyler.hall@gmail.com>
2022-10-06 11:06:45 -0400

    added console logs to inline_js and external.js

--- js/external.js
+++ js/external.js
@@ -1,11 +1,1 @@
-"use strict"
-
-console.log("Hello from Codeup!");
-
-alert("Happy Thorsday, Xerxes!")
-
-var confirmed = confirm('Are you sure you want to do XYZ?');
-console.log(confirmed);
-
-var userInput = prompt('Please type something:');
-console.log('The user entered: ' + userInput);
+console.log("Hello from external JavaScript")
