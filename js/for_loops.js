0350e41bbddc30e47e1cad86cae9f03b5708d40d
Brandon Hall <bran.tyler.hall@gmail.com>
2022-10-14 12:42:22 -0400

    created even number function

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,22 @@
-// multiplication table function
-function showMultiplicationTable(x) {
+//multiplication table function
+// function showMultiplicationTable(x) {
+//     for (let i = 1; i < 11; i++) {
+//         console.log(x + " x " + i + " = " + (i * x));
+//     }
+// }
+//
+// showMultiplicationTable(7)
+
+//random even number function
+function evenNum() {
     for (let i = 1; i < 11; i++) {
-        console.log(x + " x " + i + " = " + (i * x));
+        let random = Math.floor((Math.random() * 200) + 20)
+        if (random % 2 === 0) {
+            console.log(random + " is even");
+        } else {
+            console.log(random + " is odd");
+        }
     }
 }
 
-showMultiplicationTable(7)
-
-
+evenNum();
