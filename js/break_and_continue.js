8cd02e40b1ce3e07d6831d1849755787bc3df72a
Brandon Hall <bran.tyler.hall@gmail.com>
2022-10-14 17:03:12 -0400

    created ask for odd function

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,4 +1,26 @@
 console.log("BREAK AND CONTINUE EXERCISE:");
 console.log("");
 
-//
+//ask for odd function
+let numberToSkip = prompt("Please enter an odd number between 1 and 50");
+
+function askForOdd() {
+    while (typeof numberToSkip != "number") {
+        numberToSkip = prompt("Please enter an odd number between 1 and 50");
+        if (numberToSkip % 2 === 1 && numberToSkip < 50 && numberToSkip > 0) {
+            break;
+        }
+    }
+
+    for (let i = 1; i < 50; i++) {
+        if (parseFloat(numberToSkip) === i) {
+            console.log("DISGUSTING, WE SKIP " + i);
+            continue
+        } else if (i % 2 === 0) {
+            continue
+        }
+        console.log("Here's a cool odd number: " + i);
+    }
+}
+
+askForOdd();
