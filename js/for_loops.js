27a2d49f7578eebd73086df9d268a577fc96fa89
Brandon Hall <bran.tyler.hall@gmail.com>
2022-10-14 12:41:37 -0400

    created multiplication table function

---
+++ js/for_loops.js
@@ -0,0 +1,10 @@
+// multiplication table function
+function showMultiplicationTable(x) {
+    for (let i = 1; i < 11; i++) {
+        console.log(x + " x " + i + " = " + (i * x));
+    }
+}
+
+showMultiplicationTable(7)
+
+
