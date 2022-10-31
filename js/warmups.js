235e5e8699ffade7c14cd81b6f5be59f8406f4d0
Brandon Hall <bran.tyler.hall@gmail.com>
2022-10-31 11:21:27 -0400

    resized text on browser, finished code

--- js/warmups.js
+++ js/warmups.js
@@ -63,3 +63,18 @@ const hamsters = [
 
 console.log(tallestHammie(hamsters));
 // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
+
+function fizzBuzz() {
+	for (let i = 1; i < 101; i++)
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("FizzBuzz");
+		} else if (i % 3 === 0) {
+			console.log("Fizz");
+		} else if (i % 5 === 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+}
+
+fizzBuzz();
